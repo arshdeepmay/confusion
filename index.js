dca7455ee2022124f78d56f0284fe3be48a8befe
Dipanshu Sharma <dipanshu.sharma0610@gmail.com>
2022-05-24 09:24:43 +0530

    more bootstrap and jQuery

---
+++ index.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $(".carousel").carousel({interval:2000});
+
+    $("#carousel-btn").on("click",function(){
+        if($("#carousel-btn").children("span").children("i").hasClass("fa-play")){
+
+            $("#carouselExampleCaptions").carousel("cycle");
+            $("#carousel-btn").children("span").children("i").removeClass("fa-play");
+            $("#carousel-btn").children("span").children("i").addClass("fa-pause");
+        }
+        else{
+            $("#carouselExampleCaptions").carousel("pause");
+            $("#carousel-btn").children("span").children("i").removeClass("fa-pause");
+            $("#carousel-btn").children("span").children("i").addClass("fa-play"); 
+        }
+    })
+});
